36d772a0c8d6e1c67c10073fc68e6ac85787ded5
Yonghee <yhlee73@gmail.com>
2014-10-25 11:53:40 +0900

    ..

--- DDP/server.R
+++ DDP/server.R
@@ -1,9 +1,26 @@
-library(shiny)
+# library(shiny)
+# shinyServer(
+#     function(input,output){
+#         
+#         output$oid1 <- renderPrint({input$id1})
+#         output$oid2 <- renderPrint({input$id2})
+#         output$odate <- renderPrint({input$date})
+#     }
+# library(shiny)
+
+# shinyServer(
+#   function(input, output) {
+#     x <- reactive({as.numeric(input$text1)+100})      
+#     output$text1 <- renderText({x()      })
+#     output$text2 <- renderText({x() + as.numeric(input$text2)})
+#   }
+# )
+
 shinyServer(
-    function(input,output){
-        
-        output$oid1 <- renderPrint({input$id1})
-        output$oid2 <- renderPrint({input$id2})
-        output$odate <- renderPrint({input$date})
+    function(input, output) {
+        x <- reactive({})      
+        output$text1 <- renderText({as.numeric(input$text1)+100  })
+        output$text2 <- renderText({as.numeric(input$text1)+100 + 
+                                        as.numeric(input$text2)})
     }
 )
--- DDP/ui.R
+++ DDP/ui.R
@@ -1,20 +1,35 @@
+# library(shiny)
+# shinyUI(pageWithSidebar(
+#     headerPanel("Hello Shiny! - #2"),
+#     sidebarPanel(
+#         numericInput('id1','Numeric Input  labeled id1',0,min=0,max=10,step=1),
+#         checkboxGroupInput("id2","Checkbox",
+#                            c("Value 1 = " = "1","Value 2 = " =  "2", "Value 3 =  "= "3")),
+#         dateInput("date","Date: ")    
+#     ),
+#     mainPanel(
+#         h3("Show Output"),
+#         h4("You enterd!"),
+#         verbatimTextOutput("oid1"),
+#         h4("You enterd!"),
+#         verbatimTextOutput("oid2"),
+#         h4("You enterd!"),
+#         verbatimTextOutput("odate")
+#     )
+# ))
+#     
+
 library(shiny)
 shinyUI(pageWithSidebar(
-    headerPanel("Hello Shiny! - #2"),
+    headerPanel("Hello Shiny!"),
     sidebarPanel(
-        numericInput('id1','Numeric Input  labeled id1',0,min=0,max=10,step=1),
-        checkboxGroupInput("id2","Checkbox",
-                           c("Value 1 = " = "1","Value 2 = " =  "2", "Value 3 =  "= "3")),
-        dateInput("date","Date: ")    
+        textInput(inputId="text1", label = "Input Text1"),
+        textInput(inputId="text2", label = "Input Text2")
     ),
     mainPanel(
-        h3("Show Output"),
-        h4("You enterd!"),
-        verbatimTextOutput("oid1"),
-        h4("You enterd!"),
-        verbatimTextOutput("oid2"),
-        h4("You enterd!"),
-        verbatimTextOutput("odate")
+        p('Output text1'),
+        textOutput('text1'),
+        p('Output text2'),
+        textOutput('text2')
     )
 ))
-    
